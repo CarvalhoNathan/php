d2c4fe0294202f9dbb3fc77751c1697ff0623163
Nathan Carvalho <67009187+CarvalhoNathan@users.noreply.github.com>
2026-08-17 09:03:50 -0300

    Add files via upload

---
+++ exemplo_função.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*função quadrado
+*retorna o quadro
+*de um número
+*/
+    function quadrado ($numero){
+        return $numero * $numero;
+    }
+?>
---
+++ testa_função.php
@@ -0,0 +1,9 @@
+<?php
+// carrega biblioteca
+include 'exemplo_função.php';
+
+// chama função quadrado()
+
+echo quadrado(2.2);
+
+?>
